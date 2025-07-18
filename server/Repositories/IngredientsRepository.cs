7169504bb61ceca274a3cb5306311b6208dc1410
AdamRB208 <adamrborup@gmail.com>
2025-07-18 16:36:51 -0600

    Created Ingredients Repository

---
+++ server/Repositories/IngredientsRepository.cs
@@ -0,0 +1,13 @@
+namespace allspice_dotnet_fullstack.Repositories;
+
+public class IngredientsRepository
+{
+  private readonly IDbConnection _db;
+
+  public IngredientsRepository(IDbConnection db)
+  {
+    _db = db;
+  }
+
+  
+}
